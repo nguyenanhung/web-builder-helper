2f2d9cf87a79e3dc0363a74f21657e37d494c49b
nguyenanhung <info@nguyenanhung.com>
2022-06-09 11:48:49 +0700

    Release version 1.0.5

--- src/ProjectInterface.php
+++ src/ProjectInterface.php
@@ -17,8 +17,8 @@ namespace nguyenanhung\WebBuilderHelper;
  */
 interface ProjectInterface
 {
-    public const VERSION       = '1.0.4';
-    public const LAST_MODIFIED = '2021-10-04';
+    public const VERSION       = '1.0.5';
+    public const LAST_MODIFIED = '2022-06-09';
     public const AUTHOR_NAME   = 'Hung Nguyen';
     public const AUTHOR_EMAIL  = 'dev@nguyenanhung.com';
     public const PROJECT_NAME  = 'Helper: Web Builder by Hung Nguyen';
