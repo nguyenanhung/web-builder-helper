bf0d3bf3cd4ca8ba7d1c31f3dae4e5b063af1e72
nguyenanhung <info@nguyenanhung.com>
2022-07-02 02:49:43 +0700

    Add html tag

--- src/ProjectInterface.php
+++ src/ProjectInterface.php
@@ -17,8 +17,8 @@ namespace nguyenanhung\WebBuilderHelper;
  */
 interface ProjectInterface
 {
-    public const VERSION       = '1.0.6';
-    public const LAST_MODIFIED = '2022-06-16';
+    public const VERSION       = '1.0.7';
+    public const LAST_MODIFIED = '2022-07-02';
     public const AUTHOR_NAME   = 'Hung Nguyen';
     public const AUTHOR_EMAIL  = 'dev@nguyenanhung.com';
     public const PROJECT_NAME  = 'Helper: Web Builder by Hung Nguyen';
